95aec3e09fdb6d01f9d10a138fdc0e0d36b3c4b8
oussama_rezkellah <orezkellah@gmail.com>
2023-04-07 14:01:54 +0000

    login template

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,13 +1,105 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="icon" type="image/png" href="images/logo-16x16.png" />
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Argon - Social Network</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Major+Mono+Display" rel="stylesheet">
+    <link href='https://cdn.jsdelivr.net/npm/boxicons@1.9.2/css/boxicons.min.css' rel='stylesheet'>
+
+    <!-- Styles -->
+    <link href="css/bootstrap/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/components.css" rel="stylesheet">
+    <link href="css/auth.css" rel="stylesheet">
+    <link href="css/forms.css" rel="stylesheet">
+    <link href="css/media.css" rel="stylesheet">
 </head>
+
 <body>
-    <h1>teeest</h1>
-    <h1>test lakher</h1>
+    <div class="row ht-100v flex-row-reverse no-gutters">
+        <div class="col-md-6 d-flex justify-content-center align-items-center">
+            <div class="signup-form">
+                <div class="auth-logo text-center mb-5">
+                    <div class="row">
+                        <div class="col-md-2">
+                            <img src="images/logo-64x64.png" class="logo-img" alt="Logo">
+                        </div>
+                        <div class="col-md-10">
+                            <p>Argon Social Network</p>
+                            <span>Design System</span>
+                        </div>
+                    </div>
+                </div>
+                <form action="" method="">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="form-group">
+                                <input type="text" class="form-control" name="" placeholder="Email Address">
+                            </div>
+                        </div>
+                        <div class="col-md-12">
+                            <div class="form-group">
+                                <input type="password" class="form-control" name="" placeholder="Password">
+                            </div>
+                        </div>
+                        <div class="col-md-12 mb-3">
+                            <a href="forgot-password.html">Forgot password?</a>
+                        </div>
+                        <div class="col-md-6">
+                            <label class="custom-control material-checkbox">
+                                <input type="checkbox" class="material-control-input">
+                                <span class="material-control-indicator"></span>
+                                <span class="material-control-description">Remember Me</span>
+                            </label>
+                        </div>
+                        <div class="col-md-6 text-right">
+                            <div class="form-group">
+                                <button type="button" class="btn btn-primary sign-up">Sign In</button>
+                            </div>
+                        </div>
+                        <div class="col-md-12 text-center mt-4">
+                            <p class="text-muted">Start using your fingerprint</p>
+                            <a href="#" class="btn btn-outline-primary btn-sm sign-up" data-toggle="modal" data-target="#fingerprintModal">Use Fingerprint</a>
+                        </div>
+                        <div class="col-md-12 text-center mt-5">
+                            <span class="go-login">Not yet a member? <a href="sign-up.html">Sign Up</a></span>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <div class="col-md-6 auth-bg-image d-flex justify-content-center align-items-center">
+            <div class="auth-left-content mt-5 mb-5 text-center">
+                <div class="weather-small text-white">
+                    <p class="current-weather"><i class='bx bx-sun'></i> <span>14&deg;</span></p>
+                    <p class="weather-city">Gyumri</p>
+                </div>
+                <div class="text-white mt-5 mb-5">
+                    <h2 class="create-account mb-3">Welcome Back</h2>
+                    <p>Thank you for joining. Updates and new features are released daily.</p>
+                </div>
+                <div class="auth-quick-links">
+                    <a href="#" class="btn btn-outline-primary">Purchase template</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    
+
+    <!-- Core -->
+    <script src="js/jquery/jquery-3.3.1.min.js"></script>
+    <script src="js/popper/popper.min.js"></script>
+    <script src="js/bootstrap/bootstrap.min.js"></script>
+    <!-- Optional -->
+    <script src="js/app.js"></script>
 </body>
+
 </html>
